ec9d6d2d1b097bb681348c8d39bbff62b0804fd0
Ieva-Briede <briede.ieva.1@gmail.com>
2021-02-03 21:15:46 +0200

    New project 1

---
+++ src/AssigmentFile.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Assigment5 {
+    public static void main(String[] args) {
+        int[] myArray = new int[20000];
+
+    }
+}
+
+
+
